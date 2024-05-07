95b827b15b2b9a3374b716c06965b8611eb3de46
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-05-07 19:49:22 +1000

    Add initial library implementation for circular memory buffer

---
+++ membuff/membuff.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "membuff.h"
+
+void MemBuff_init(MemBuff* mem, uint8_t* buff, int buffSize, int pageSize) {
+  mem->buff = buff;
+  mem->buffSize = buffSize;
+  mem->pageSize = pageSize;
+  mem->length = 0;
+  mem->head = mem->buff;                    // Initialise head at buff[0] 
+  mem->cell = mem->head;                    // Initialise cell pointer at head
+  mem->tail = mem->head + mem->pageSize-1;  // Initialise tail incremented a page length from head
+  mem->buffEnd = mem->buff + mem->buffSize-1;
+
+  mem->_slide = _MemBuff_slide;
+
+  mem->flush = MemBuff_flush;
+  mem->append = MemBuff_append;
+  mem->erase = MemBuff_erase;
+}
+
+/******************** INTERNAL INTERFACE ********************/
+void _MemBuff_slide(MemBuff* mem, uint8_t* newHead) {
+  bool headOverflow = (newHead >= mem->buffEnd);
+  bool tailOverflow = (newHead + mem->pageSize) >= mem->buffEnd;
+
+  if (headOverflow) {
+    // If head extends beyond the buffer
+    int diff = (int)(newHead - mem->buffEnd);
+    mem->head = mem->buff + diff;           // New head loops back difference from start of buffer
+    mem->tail = mem->head + mem->pageSize;  // New tail is simply a page length increment from new head
+  } else if (tailOverflow) {
+    // If tail extends beyond the buffer
+    int diff = (int)((newHead + mem->pageSize) - mem->buffEnd);
+    mem->head = newHead;                    // New head is simply the passed pointer
+    mem->tail = mem->buff + diff;           // New tail loops back difference from start of buffer
+  } else {
+    // Base case
+    mem->head = newHead;
+    mem->tail = newHead + mem->pageSize;
+  }
+}
+
+/********************* PUBLIC INTERFACE *********************/
+void MemBuff_append(MemBuff* mem, uint8_t data) {
+  // Circle cell pointer back to start of buffer if past end
+  if (mem->cell == mem->buffEnd) {
+    mem->cell = mem->buff;
+  }
+
+  *mem->cell = data;
+  mem->cell++;
+  mem->length++;
+}
+
+void MemBuff_flush(MemBuff* mem) {
+  // Slide head to next page window
+  if (mem->length >= mem->pageSize) {
+    // If cell has overflowed page window
+    mem->_slide(mem, (mem->head + mem->pageSize));  // Push head and tail forward a page length
+    mem->length = (mem->cell - mem->head);          // Calculate new length from cell overflow
+  } else {
+    // If cell is still within page window
+    mem->_slide(mem, mem->cell);                    // Push head and tail forward to start at current cell
+    mem->length = 0;                                // Reset length to 0
+  }
+
+  // Erase page window from current cell onward
+  mem->erase(mem, mem->cell, mem->tail);
+}
+
+void MemBuff_erase(MemBuff* mem, uint8_t* start, uint8_t* end) {
+  int diff = (int)(end - start);
+  uint8_t* cell = start;
+  for(int i = 0; i < diff; i++) {
+    // Loop back to start of buffer on overflow
+    if (cell >= mem->buffEnd)
+        cell = mem->buff;
+    *cell = 0;
+    cell++;
+  }
+}
---
+++ membuff/membuff.h
@@ -0,0 +1,26 @@
+#ifndef _MEMBUFF_H
+#define _MEMBUFF_H
+
+typedef struct MemBuff {
+  uint8_t* buff;
+  uint8_t* buffEnd; // Last cell in buffer
+  uint8_t* head;    // Page window head
+  uint8_t* tail;    // Page window tail
+  uint8_t* cell;    // Current cell in buffer
+  int pageSize; 
+  int buffSize;
+  int length; 
+  void (*_slide)(struct MemBuff*, uint8_t*);
+  void (*flush)(struct MemBuff*);
+  void (*append)(struct MemBuff*, uint8_t);
+  void (*erase)(struct MemBuff*, uint8_t*, uint8_t*);
+} MemBuff;
+
+void _MemBuff_slide(MemBuff*, uint8_t*);
+
+void MemBuff_init(MemBuff*, uint8_t*, int, int);
+void MemBuff_flush(MemBuff*);
+void MemBuff_append(MemBuff*, uint8_t);
+void MemBuff_erase(struct MemBuff*, uint8_t*, uint8_t*);
+
+#endif
