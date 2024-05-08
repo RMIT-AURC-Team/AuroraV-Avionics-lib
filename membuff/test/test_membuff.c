c565a1c4de647e8ec52e42ffec9874ec087fb79e
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-05-08 23:35:50 +1000

    Update test structure and test cases

--- membuff/test/test_membuff.c
+++ membuff/test/test_membuff.c
@@ -1,37 +1,62 @@
+#include <stdbool.h>
+#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "membuff.h"
 
-int main() {
+bool assert_buffer_equals(char* test, uint8_t* buff, uint8_t* goalBuff, int buffSize) {
+  printf("Running %s\n", test);
+  printf("Test buffer:\t ");
+  for(int i = 0; i < buffSize; i++)
+    printf("%d ", buff[i]);
+  printf("\n");
+
+  printf("Goal buffer:\t ");
+  for(int i = 0; i < buffSize; i++)
+    printf("%d ", goalBuff[i]);
+  printf("\n");
+
+  return memcmp(buff, goalBuff, buffSize) ? 0 : 1; // memcmp returns 0 if equal
+}
+
+bool test_append_no_overflow() {
   MemBuff mem;
-  uint8_t buff[12];
-  MemBuff_init(&mem, buff, sizeof(buff), 4);
+  int buffSize = 12;
+  int pageSize = 4;
 
-  for(int i = 0; i < 48; i++) {
-    if (i%5 == 0) {
-      printf("Current head: %p\t", mem.head);
-      printf("Current cell: %p\t", mem.cell);
-      printf("Current tail: %p\n", mem.tail);
+  uint8_t buff[buffSize];
+  MemBuff_init(&mem, buff, buffSize, pageSize);
 
-      uint8_t flush[mem.pageSize];
-      mem.flush(&mem, flush);
-      for(int j = 0; j < sizeof(flush); j++)
-          printf("%d ", flush[j]);
-
-      printf("\n");
-      printf("New head: %p\t", mem.head);
-      printf("New cell: %p\t", mem.cell);
-      printf("New tail: %p\n", mem.tail);
-      printf("\n\n");
-    }
-
-    int x = i%12 ? i : i+10;
-    mem.append(&mem, x);
+  // Write numbers 0-11 to buffer
+  for(int i = 0; i < buffSize; i++) {
+    mem.append(&mem, i);  // Append integer to buffer
   }
+  
+  uint8_t goalBuff[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+  return assert_buffer_equals("test_append_no_overflow\0", buff, goalBuff, buffSize);
+}
 
-  printf("\n\n");
-  for(int i = 0; i < 12; i++)
-    printf("%d ", mem.buff[i]);
+bool test_append_overflow() {
+  MemBuff mem;
+  int buffSize = 12;
+  int pageSize = 4;
+
+  uint8_t buff[buffSize];
+  MemBuff_init(&mem, buff, buffSize, pageSize);
+
+  // Write numbers 0-11 to buffer
+  for(int i = 0; i < 2*buffSize; i++) {
+    mem.append(&mem, i);  // Append integer to buffer
+  }
+  
+  uint8_t goalBuff[] = {12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+  return assert_buffer_equals("test_append_overflow\0", buff, goalBuff, buffSize);
+}
+
+int main() {
+  bool (*tests[])() = {test_append_no_overflow, test_append_overflow}; 
+  for(int i = 0; i < sizeof(tests); i++)
+    printf("Test %s\n\n", tests[i]() ? "passed" : "failed");
 
   return 0;
 }
