0caf101b4e6a124e6b68f072819e571ee6b3f1f9
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-06-09 21:41:00 +1000

    Minor formatting change to test file

--- membuff/test/test_membuff.c
+++ membuff/test/test_membuff.c
@@ -2,40 +2,8 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "libtest.h"
 #include "membuff.h"
-
-bool assert(char* test, bool assertion, bool value) {
-  if (test != NULL)
-    printf("Asserting %s\n", test);
-  return assertion == value;
-}
-
-bool assert_buffer_equals(char* test, uint8_t* buff, uint8_t* goalBuff, int buffSize) {
-  if (test != NULL)
-    printf("Asserting %s\n", test);
-
-  printf("Test buffer:\t ");
-  for(int i = 0; i < buffSize; i++)
-    printf("%d ", buff[i]);
-  printf("\n");
-
-  printf("Goal buffer:\t ");
-  for(int i = 0; i < buffSize; i++)
-    printf("%d ", goalBuff[i]);
-  printf("\n\n");
-
-  return memcmp(buff, goalBuff, buffSize) ? 0 : 1; // memcmp returns 0 if equal
-}
-
-bool assert_pointer_equals(char* test, uint8_t* point, uint8_t* goalPoint) {
-  if (test != NULL)
-    printf("Asserting %s\n", test);
-
-  printf("Test pointer:\t%p\n", point);
-  printf("Goal pointer:\t%p\n\n", goalPoint);
-
-  return point == goalPoint;
-}
 
 /**************************** APPEND TESTS ****************************/
 
@@ -216,12 +184,11 @@ int main() {
   bool result = false;
   for(int i = 0; i < numTests; i++) {
     printf("--------------------------------------\n\n");
-    bool result = tests[i]();
+    result = tests[i]();
     printf("Test %s\n\n", result ? "passed" : "failed");
     passCount += result ? 1 : 0;
   }
   printf("--------------------------------------\n");
-  printf("        |  Test summary  |\n\n");
   printf("number of tests: %d\ntests passed: %d\n\n", numTests, passCount);
 
 
