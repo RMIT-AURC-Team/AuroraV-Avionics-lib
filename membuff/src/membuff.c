57ba7c5a9552921949ae6e3bc006a59c3b26525e
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-07-04 16:38:13 +1000

    Update length calculation on append to not overflow buffer size, fix test case erroneously expecting a flush success on empty buffer, formatting adjustments

--- membuff/src/membuff.c
+++ membuff/src/membuff.c
@@ -1,46 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 #include "membuff.h"
 
-void MemBuff_init(MemBuff* mem, uint8_t* buff, int buffSize, int pageSize) {
-  mem->buff = buff;
+void MemBuff_init(MemBuff *mem, uint8_t *buff, int buffSize, int pageSize) {
+  mem->buff     = buff;
   mem->buffSize = buffSize;
   mem->pageSize = pageSize;
-  mem->length = 0;
-  mem->head = mem->buff;                    // Initialise head at buff[0] 
-  mem->cell = mem->head;                    // Initialise cell pointer at head
-  mem->tail = mem->head + mem->pageSize-1;  // Initialise tail incremented a page length from head
-  mem->buffEnd = mem->buff + mem->buffSize;
+  mem->length   = 0;
+  mem->head     = mem->buff;                     // Initialise head at buff[0]
+  mem->cell     = mem->head;                     // Initialise cell pointer at head
+  mem->tail     = mem->head + mem->pageSize - 1; // Initialise tail incremented a page length from head
+  mem->buffEnd  = mem->buff + mem->buffSize;
 
-  mem->_slide = _MemBuff_slide;
+  mem->_slide    = _MemBuff_slide;
   mem->pageReady = false;
 
   mem->readPage = MemBuff_readPage;
-  mem->flush = MemBuff_flush;
-  mem->append = MemBuff_append;
-  mem->erase = MemBuff_erase;
+  mem->flush    = MemBuff_flush;
+  mem->append   = MemBuff_append;
+  mem->erase    = MemBuff_erase;
 }
 
 /******************** INTERNAL INTERFACE ********************/
 
-void _MemBuff_slide(MemBuff* mem, uint8_t* newHead) {
-  int pageEnd = mem->pageSize - 1;
+void _MemBuff_slide(MemBuff *mem, uint8_t *newHead) {
+  int pageEnd       = mem->pageSize - 1;
   bool headOverflow = (newHead >= mem->buffEnd);
   bool tailOverflow = (newHead + pageEnd) >= mem->buffEnd;
 
   if (headOverflow) {
     // If head extends beyond the buffer
-    int diff = (int)(newHead - mem->buffEnd);
-    mem->head = mem->buff + diff;           // New head loops back difference from start of buffer
-    mem->tail = mem->head + pageEnd;        // New tail is simply a page length increment from new head
+    int diff  = (int)(newHead - mem->buffEnd);
+    mem->head = mem->buff + diff;    // New head loops back difference from start of buffer
+    mem->tail = mem->head + pageEnd; // New tail is simply a page length increment from new head
   } else if (tailOverflow) {
     // If tail extends beyond the buffer
-    int diff = (int)((newHead + pageEnd) - mem->buffEnd);
-    mem->head = newHead;                    // New head is simply the passed pointer
-    mem->tail = mem->buff + diff;           // New tail loops back difference from start of buffer
+    int diff  = (int)((newHead + pageEnd) - mem->buffEnd);
+    mem->head = newHead;          // New head is simply the passed pointer
+    mem->tail = mem->buff + diff; // New tail loops back difference from start of buffer
   } else {
     // Base case
     mem->head = newHead;
@@ -55,7 +53,7 @@ void _MemBuff_slide(MemBuff* mem, uint8_t* newHead) {
  * - Adds data to the next cell in the internal buffer, handles overflow.
  * - Increments length after append and sets flag if page window is full;
  * =============================================================================== */
-void MemBuff_append(MemBuff* mem, uint8_t data) {
+void MemBuff_append(MemBuff *mem, uint8_t data) {
   // Circle cell pointer back to start of buffer if past end
   if (mem->cell == mem->buffEnd) {
     mem->cell = mem->buff;
@@ -65,9 +63,9 @@ void MemBuff_append(MemBuff* mem, uint8_t data) {
   mem->cell++;
 
   // Increment length and set ready flag if page window is filled
-  ++mem->length;
+  mem->length += (mem->length >= mem->buffSize) ? 0 : 1; // Don't increment on full buffer
   if (mem->length >= mem->pageSize) {
-    mem->pageReady = true;          
+    mem->pageReady = true;
   }
 }
 
@@ -76,29 +74,29 @@ void MemBuff_append(MemBuff* mem, uint8_t data) {
  * - Flushes data to output buffer if page window is ready to be read
  *    and resets flag.
  * =============================================================================== */
-bool MemBuff_readPage(MemBuff* mem, uint8_t* outBuff) {
+bool MemBuff_readPage(MemBuff *mem, uint8_t *outBuff) {
   // Early exit with false if no page ready
   if (!mem->pageReady) {
     return false;
   }
 
-  mem->flush(mem, outBuff);  // Flush to output buffer
+  mem->flush(mem, outBuff); // Flush to output buffer
   return true;
 }
 
 /* ===============================================================================
  * FLUSH
- * - Copies out data currently within page window to output buffer and 
+ * - Copies out data currently within page window to output buffer and
  *    slides page window forward.
  * - Used by readPage function to handle guarded output, can be used standalone
  *    for early buffer reads.
  *
  * Caution should be taken when calling directly, problems can occur if
- *    the internal buffer overflows back to the current window. It is recommended 
+ *    the internal buffer overflows back to the current window. It is recommended
  *    to use readPage where possible to flush out the window when full.
  * =============================================================================== */
-bool MemBuff_flush(MemBuff* mem, uint8_t* outBuff) {
- // Early exit with false if nothing to flush
+bool MemBuff_flush(MemBuff *mem, uint8_t *outBuff) {
+  // Early exit with false if nothing to flush
   if (mem->length == 0) {
     return false;
   }
@@ -108,11 +106,11 @@ bool MemBuff_flush(MemBuff* mem, uint8_t* outBuff) {
 
   // Copy data from page window to output buffer
   if (mem->head + flushSize <= mem->buffEnd) {
-    // If flush window is within the buffer 
-    memcpy(outBuff, mem->head, flushSize);   // Copy data from head to tail
+    // If flush window is within the buffer
+    memcpy(outBuff, mem->head, flushSize); // Copy data from head to tail
   } else {
     // If flush window overflows buffer
-    int diff = (int)(mem->buffEnd - mem->head);
+    int diff      = (int)(mem->buffEnd - mem->head);
     int startDiff = flushSize - diff;
     memcpy(outBuff, mem->head, diff);             // Copy data from head to end of buffer
     memcpy(outBuff + diff, mem->buff, startDiff); // Copy data from start of buffer to tail
@@ -120,9 +118,9 @@ bool MemBuff_flush(MemBuff* mem, uint8_t* outBuff) {
 
   // Slide head to next page window or to cell
   if (flushSize == mem->pageSize) {
-    mem->_slide(mem, mem->head + mem->pageSize);  // Push head and tail forward a page length
+    mem->_slide(mem, mem->head + mem->pageSize); // Push head and tail forward a page length
   } else {
-    mem->_slide(mem, mem->cell);  // Push head to the current cell position
+    mem->_slide(mem, mem->cell);                 // Push head to the current cell position
   }
 
   // Update length
@@ -142,13 +140,13 @@ bool MemBuff_flush(MemBuff* mem, uint8_t* outBuff) {
  * - Set cells to zero between start and end pointers.
  * - Used by the flush function to prevent writing out old data to memory.
  * =============================================================================== */
-void MemBuff_erase(MemBuff* mem, uint8_t* start, uint8_t* end) {
-  int diff = (int)(end - start);
-  uint8_t* cell = start;
-  for(int i = 0; i <= diff; i++) {
+void MemBuff_erase(MemBuff *mem, uint8_t *start, uint8_t *end) {
+  int diff      = (int)(end - start);
+  uint8_t *cell = start;
+  for (int i = 0; i <= diff; i++) {
     // Loop back to start of buffer on overflow
     if (cell >= mem->buffEnd)
-        cell = mem->buff;
+      cell = mem->buff;
     *cell = 0;
     cell++;
   }
--- membuff/test/test_membuff.c
+++ membuff/test/test_membuff.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "libtest.h"
 #include "membuff.h"
 
@@ -16,10 +16,10 @@ bool test_append_no_overflow() {
   MemBuff_init(&mem, buff, buffSize, pageSize);
 
   // Write numbers 0-11 to buffer
-  for(int i = 0; i < buffSize; i++) {
-    mem.append(&mem, i);  // Append integer to buffer
+  for (int i = 0; i < buffSize; i++) {
+    mem.append(&mem, i); // Append integer to buffer
   }
-  
+
   uint8_t goalBuff[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
   ASSERT_BUFF_EQ("test_append_no_overflow", buff, goalBuff, sizeof(buff), "d")
   return true;
@@ -34,10 +34,10 @@ bool test_append_overflow() {
   MemBuff_init(&mem, buff, buffSize, pageSize);
 
   // Write numbers 0-11 to buffer
-  for(int i = 0; i < 2*buffSize; i++) {
-    mem.append(&mem, i);  // Append integer to buffer
+  for (int i = 0; i < 2 * buffSize; i++) {
+    mem.append(&mem, i); // Append integer to buffer
   }
-  
+
   uint8_t goalBuff[] = {12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
   ASSERT_BUFF_EQ("test_append_overflow", buff, goalBuff, sizeof(buff), "d")
   return true;
@@ -76,7 +76,7 @@ bool test_flush_simple_case() {
   }
 
   uint8_t goalBuff[] = {0, 1, 2, 3};
-  uint8_t outBuff[pageSize]; 
+  uint8_t outBuff[pageSize];
   bool pageReady = mem.readPage(&mem, outBuff);
 
   // Assert flushed data matches goal and window slides forward a page length
@@ -113,27 +113,26 @@ bool test_flush_wrap_around() {
   bool pageReady = mem.readPage(&mem, outBuff);
   ASSERT("page ready", pageReady, true)
   ASSERT_EQ("page head", mem.head, buff + pageSize, "p")
-  ASSERT_EQ("page tail", mem.tail, buff + 2*pageSize-1, "p")
+  ASSERT_EQ("page tail", mem.tail, buff + 2 * pageSize - 1, "p")
   ASSERT_BUFF_EQ("first output buffer", outBuff, goalBuff1, pageSize, "d")
 
   // Second read, window should slide a page length  and loop back
   // with correct data. Page should be ready to read
   uint8_t goalBuff2[] = {0, 0, 0, 0};
-  pageReady = mem.readPage(&mem, outBuff);
+  pageReady           = mem.readPage(&mem, outBuff);
   ASSERT("page ready", pageReady, true)
   ASSERT_EQ("page head", mem.head, buff, "p")
-  ASSERT_EQ("page tail", mem.tail, buff + pageSize-1, "p")
+  ASSERT_EQ("page tail", mem.tail, buff + pageSize - 1, "p")
   ASSERT_BUFF_EQ("second output buffer", outBuff, goalBuff2, pageSize, "d")
 
-  // Third read, window should partial slide two cells with
-  // with correct data. Page should be not ready to read, 
-  // flush should succeed
-  pageReady = mem.readPage(&mem, outBuff);
+  // Third read, window should not slide, page should be not ready to read,
+  // flush should fail due to insufficient data (0 after full buffer flush)
+  pageReady    = mem.readPage(&mem, outBuff);
   bool flushed = mem.flush(&mem, outBuff);
   ASSERT("page not ready", pageReady, false)
-  ASSERT("flush success", flushed, true)
-  ASSERT_EQ("page head", mem.head, buff + 2, "p")
-  ASSERT_EQ("page tail", mem.tail, buff + 2 + pageSize-1, "p")
+  ASSERT("flush success", flushed, false)
+  ASSERT_EQ("page head", mem.head, buff, "p")
+  ASSERT_EQ("page tail", mem.tail, buff + pageSize - 1, "p")
 
   return true;
 }
@@ -154,7 +153,7 @@ bool test_flush_partial_window() {
   uint8_t goalBuff[] = {0, 1};
   uint8_t outBuff[buffSize];
   bool pageReady = mem.readPage(&mem, outBuff);
-  bool flushed = mem.flush(&mem, outBuff);
+  bool flushed   = mem.flush(&mem, outBuff);
 
   // Assert flushed data matches partial window and window slides forward by 2 cells
   printf("Running test_flush_partial_window\n");
@@ -191,7 +190,7 @@ bool test_flush_overflowed_window() {
 
   ASSERT("page ready", pageReady, true)
   ASSERT_EQ("page head", mem.head, buff + pageSize, "p")
-  ASSERT_EQ("page tail", mem.tail, buff + 2*pageSize - 1, "p")
+  ASSERT_EQ("page tail", mem.tail, buff + 2 * pageSize - 1, "p")
   ASSERT_BUFF_EQ("output buffer", outBuff, goalBuff, pageSize, "d")
   return true;
 }
@@ -217,11 +216,11 @@ bool test_flush_multiple_overflow() {
   bool pageReady = mem.readPage(&mem, outBuff);
   ASSERT("page ready", pageReady, true)
   ASSERT_EQ("page head", mem.head, buff + pageSize, "p")
-  ASSERT_EQ("page tail", mem.tail, buff + 2*pageSize - 1, "p")
+  ASSERT_EQ("page tail", mem.tail, buff + 2 * pageSize - 1, "p")
   ASSERT_BUFF_EQ("output buffer", outBuff, goalBuff, pageSize, "d")
 
   uint8_t goalBuff2[] = {4, 5, 6, 7};
-  pageReady = mem.readPage(&mem, outBuff);
+  pageReady           = mem.readPage(&mem, outBuff);
   ASSERT("page ready", pageReady, true)
   ASSERT_EQ("page head", mem.head, buff, "p")
   ASSERT_EQ("page tail", mem.tail, buff + pageSize - 1, "p")
@@ -230,7 +229,7 @@ bool test_flush_multiple_overflow() {
   pageReady = mem.readPage(&mem, outBuff);
   ASSERT("page not ready", mem.pageReady, false)
   ASSERT("page read failure", pageReady, false)
-  
+
   return true;
 }
 
@@ -238,12 +237,12 @@ bool test_flush_multiple_overflow() {
 
 int main() {
   TestFunction tests[] = {
-    test_append_no_overflow, test_append_overflow,
-    test_flush_empty_buffer, test_flush_simple_case, 
-    test_flush_wrap_around, test_flush_partial_window, test_flush_overflowed_window,
-    test_flush_multiple_overflow
-  }; 
-  int numTests = sizeof(tests)/sizeof(TestFunction);
+      test_append_no_overflow, test_append_overflow,
+      test_flush_empty_buffer, test_flush_simple_case,
+      test_flush_wrap_around, test_flush_partial_window, test_flush_overflowed_window,
+      test_flush_multiple_overflow
+  };
+  int numTests = sizeof(tests) / sizeof(TestFunction);
   run_tests(numTests, tests);
 
   return 0;
